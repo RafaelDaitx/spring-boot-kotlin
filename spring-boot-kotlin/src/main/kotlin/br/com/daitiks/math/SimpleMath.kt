10aed41686eaed5c2e2636f927af3e0e0f06e15f
Rafael Silva <rafael.cervantestecnologia@gmail.com>
2023-02-28 00:38:47 -0300

    refatoração

---
+++ spring-boot-kotlin/src/main/kotlin/br/com/daitiks/math/SimpleMath.kt
@@ -0,0 +1,13 @@
+package br.com.daitiks.math
+
+import java.lang.Math.sqrt
+
+class SimpleMath {
+
+    fun sum(numberOne: Double, numberTwo: Double) = numberOne + numberTwo
+    fun subtractiob(numberOne: Double, numberTwo: Double) = numberOne - numberTwo
+    fun multiplication(numberOne: Double, numberTwo: Double) = numberOne * numberTwo
+    fun division(numberOne: Double, numberTwo: Double) = numberOne / numberTwo
+    fun mean(numberOne: Double, numberTwo: Double) = (numberOne + numberTwo) / 2
+    fun squareRoot(number: Double) = sqrt(number)
+}
