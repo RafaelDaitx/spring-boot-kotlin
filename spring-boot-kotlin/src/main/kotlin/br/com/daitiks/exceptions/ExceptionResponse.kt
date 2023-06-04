4dbfd303d060a89fa636ce478544cdb7f852661f
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2023-06-04 17:44:33 -0300

    Update

--- spring-boot-kotlin/src/main/kotlin/br/com/daitiks/exceptions/ExceptionResponse.kt
+++
@@ -1,9 +0,0 @@
-package br.com.daitiks.exceptions
-
-import java.util.*
-
-class ExceptionResponse(
-    val timeStamp: Date,
-    val message: String?,
-    val details: String
-)
--- spring-boot-kotlin/src/main/kotlin/br/com/daitiks/exceptions/UnsorpportedMathOperationException.kt
+++
@@ -1,6 +0,0 @@
-package br.com.daitiks.exceptions
-import java.lang.*
-import kotlin.RuntimeException
-
-class UnsorpportedMathOperationException(exception: String?) : RuntimeException(exception) {
-}
