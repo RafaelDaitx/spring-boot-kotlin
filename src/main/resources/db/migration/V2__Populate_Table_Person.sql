41ce2c159a2a0f619f2acd095832ac145a88c1ee
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2023-06-04 21:29:04 -0300

    Implementation Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+  (1, 'porbandar: india', 'Mhuamad', 'Male', 'Ghandi'),
+  (2, 'Mvezo- South Africa', 'Nelson ', 'Male', 'Mandela'),
+  (3, 'Mvezo- South Africa', 'dfsfds ', 'Male', 'Mandela');
+
