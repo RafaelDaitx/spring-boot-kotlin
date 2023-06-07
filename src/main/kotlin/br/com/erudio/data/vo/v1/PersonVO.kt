95a2e3d38134df47be6c42a4fa0ace7462575034
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2023-06-07 00:14:54 -0300

    Change the name of fields on Json

--- src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -1,11 +1,18 @@
 package br.com.erudio.data.vo.v2
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import java.util.Date
 
+@JsonPropertyOrder("id", "address", "firstname", "lastname", "gender")
 data class PersonVO (
 
     var id: Long = 0,
+
+  //  @field:JsonProperty("first_name") -> alterar nome do campo
     var firstName: String = "",
+
+  //  @field:JsonProperty("last_name") -> alterar nome do campo
     var lastName: String = "",
     var address: String = "",
     var gender: String = "",
